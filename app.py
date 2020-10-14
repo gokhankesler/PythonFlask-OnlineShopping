f0bcd24d3c16b58362b25519d920209716891239
Gokhan KESLER <gokhankesler@gmail.com>
2020-10-14 21:22:20 +0300

    update

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, g, flash
 #import pdb
 import sqlite3
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secretkey"
+
+
+class NewItemForm(FlaskForm):
+    title = StringField("Title")
+    price = StringField("Price")
+    description = TextAreaField("Description")
+    submit = SubmitField("Submit")
 
 
 @app.route("/")
@@ -38,16 +47,16 @@ def home():
 def new_item():
     conn = get_db()
     c = conn.cursor()
-
+    form = NewItemForm()
     if request.method == "POST":
         # Process the form data
         c.execute("""INSERT INTO items
                     (title, description, price, image, category_id, subcategory_id)
                         VALUES (?,?,?,?,?,?)""",
                   (
-                      request.form.get("title"),
-                      request.form.get("description"),
-                      float(request.form.get("price")),
+                      form.title.data,
+                      form.description.data,
+                      float(form.price.data),
                       "",
                       1,
                       1
@@ -58,7 +67,7 @@ def new_item():
         flash(f"Item {request.form.get('title')} has been successfully submitted.", "success")
         return redirect(url_for("home"))
 
-    return render_template("new_item.html")
+    return render_template("new_item.html", form=form)
 
 
 def get_db():
