819648d3098cdc8ca7cdfef51fb24fca8c71eb8e
Gokhan KESLER <gokhankesler@gmail.com>
2020-10-14 17:02:05 +0300

    new item add form added

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect,url_for
+#import pdb
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/item/new")
+
+@app.route("/item/new", methods=['GET', 'POST'])
 def new_item():
+    if request.method == "POST":
+        print("Form Data:")
+        print(f"Title: {request.form['title']}, Description: {request.form['description']}")
+
+        # Redirect to some page
+        return redirect(url_for("home"))
     return render_template("new_item.html")
